0dd3e00b6c120f8dc1307524a161bd5fcdeae8f1
Hayden Smith <mail@chambalam.com>
2019-06-11 12:37:26 +1000

    Week 2 updated

--- tut/02/solutions/iteratorclass.cpp
+++ tut/02/solutions/iteratorclass.cpp
@@ -13,7 +13,7 @@ forward_li.cbegin();  // const forward iterator
 (*forward_li.cbegin())++;  // fails to compile (attempt to modify non-const iterator) 
 forward_li.crbegin();  // Fails to compile (cannot get reverse iterator for forward iterators) 
 s.begin();  // const random access iterator 
-std::back_inserter(vec);  // fails to compile (trying to create non-const iterator for const vector) 
+std::back_inserter(vec);  // Compiles. Can't invoke the dereference operator to insert anything because you get a compile time error.
 std::back_inserter(li);  // Output iterator 
 std::istream_iterator<int>(std::cin);  // Input iterator 
 std::ostream_iterator<int>(std::cout, " ");  // Output iterator 
