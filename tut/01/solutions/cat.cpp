b4c04ca1fc212ea8295e77959b4505e3098efbf9
Hayden Smith <mail@chambalam.com>
2019-06-18 11:25:46 +1000

    Spec extension, wordladder; cat solution update

--- tut/01/solutions/cat.cpp
+++ tut/01/solutions/cat.cpp
@@ -2,8 +2,20 @@
 #include <string>
 
 int main() {
+  // a std::string is a dynamically-sized character array, which
+  //  means we are not required to worry about the fixed-sized arrays
+  //  that we're used to in C. In this way it makes it harder to overflow
+  //  a char array or waste memory. std::string is also a class meaning
+  //  a lot of important pieces of data associated with the types are stored in
+  //  the object itself (e.g. size). Strings also have the added benefits
+  //  of providing an iterator to loop through the string 
   std::string buffer;
+
+
+  //
   std::getline(std::cin, buffer);
+
+  //
   std::cout << buffer << "\n";
   return 0;
 }
