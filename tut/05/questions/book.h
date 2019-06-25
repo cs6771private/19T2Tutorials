0d36063a82df1658af5ccb4ebb77b67c987b9925
Hayden Smith <mail@chambalam.com>
2019-06-26 01:25:37 +1000

    Week 5 added, week 4 solutions added

---
+++ tut/05/questions/book.h
@@ -0,0 +1,10 @@
+class Book {
+public:
+  int getIsbn() const { return isbn; }
+  double getPrice() const  { return price; }
+private:
+  std::string name;
+  std::string author;
+  int isbn;
+  double price;
+};
