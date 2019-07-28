dfda09de186ea22b0e1aea599b8ab418af04c309
Hayden Smith <mail@chambalam.com>
2019-07-28 23:19:09 +1000

    Week9 finish

---
+++ tut/09/solutions/forward.cpp
@@ -0,0 +1,20 @@
+template <typename T, typename... Args>
+auto my_make_unique(T item) {
+  return std::unique_ptr<T>{new T{item}};
+}
+
+int main() {
+  MyClass myClass{"MyClass"};
+  std::cout << *my_make_unique<MyClass>(myClass) << "\n";
+  std::cout << *my_make_unique<MyClass>(std::move(myClass)) << "\n";
+  NonCopyable nonCopyable{"NonCopyable"};
+  std::cout << *my_make_unique<NonCopyable>(std::move(nonCopyable)) << "\n\n";
+
+  // Use the size constructor.
+  std::cout << my_make_unique<std::vector<MyClass>>(5U)->size() << "\n";
+
+  // Use the size-and-value constructor.
+  MyClass base{"hello"};
+  std::cout << my_make_unique<std::vector<MyClass>>(6U, base)->size() << "\n";
+}
+
