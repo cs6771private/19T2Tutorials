2c229a5763fcc87405e2aa72de8ab2595bb781f9
Hayden Smith <mail@chambalam.com>
2019-08-24 22:33:13 +1000

    Fix for Q1

--- tut/00/questions/1.cpp
+++ tut/00/questions/1.cpp
@@ -3,14 +3,14 @@
 
 class Lemon {
  public:
-  explicit Lemon(double r, std::string s) : sourness_{s}, radius_{r} {}
-  friend operator<<(ostream& os, const Lemon& l) { os << r; }
+  explicit Lemon(const int r, std::string s) : sourness_{s}, radius_{r} {}
+  friend std::ostream& operator<<(std::ostream& os, const Lemon& l) { os << l.radius_; return os; }
  private:
   int radius_;
   std::string sourness_;
-}
+};
 
 int main() {
-  Lemon l(3.0, "Very sour!");
+  Lemon l(3, "Very sour!");
   std::cout << l << "\n";
 }
